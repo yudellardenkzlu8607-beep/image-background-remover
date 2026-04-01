dd93fbef47e48e85cbe949fd38ab3b042a993a24
yudellardenkzlu8607-beep <yudellardenkzlu8607-beep@users.noreply.github.com>
2026-04-01 14:18:56 +0800

    remove functions from out - use _worker.js only

--- out/functions/api/auth/signout.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestPost(context) {
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: '/',
-      'Set-Cookie': 'session=; HttpOnly; Path=/; SameSite=Lax; Secure; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
-    },
-  });
-}
